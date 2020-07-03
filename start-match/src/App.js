107bc453d8c8c6b704276a25985a6c0e478aa3b0
Trieu Ngo Quang <trieu.ngoquang@vti.com.vn>
2020-07-03 14:17:54 +0700

    game start-match

---
+++ start-match/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import StarMatch from './views/StartMatch';
+
+
+function App() {
+  return (
+    <div>
+      <StarMatch />
+    </div>
+  );
+}
+
+export default App;
