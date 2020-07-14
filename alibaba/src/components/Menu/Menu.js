380e0d88f18d9798135c8f6bc54cc22eb6286983
Trieu Ngo Quang <trieu.ngoquang@vti.com.vn>
2020-07-14 16:32:26 +0700

    done routes

--- alibaba/src/components/Menu/Menu.js
+++ alibaba/src/components/Menu/Menu.js
@@ -1,4 +1,40 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+
+const menus = [
+    {
+        name: 'Home',
+        to: '/',
+        exact: true
+    },
+    {
+        name: 'Product Managements',
+        to: '/product-list',
+        exact: false
+    }
+];
+
+const MenuLink = ({label, to, activeOnlyWhenExact}) => {
+    return (
+        <Route
+            path={to}   
+            exact={activeOnlyWhenExact}
+            children={({match}) => {
+                var active = match ? 'active' : '';
+                return (
+                    <li className={`nav-item ${active}`}>
+                        <Link
+                            to={to}
+                            className="nav-link">
+                            {label}
+                        </Link>
+                    </li>
+                );
+            }}
+        />
+    )
+}
 
 class Menu extends Component {
     render() {
@@ -10,16 +46,28 @@ class Menu extends Component {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <span className="nav-link">Home <span className="sr-only">(current)</span></span>
-                        </li>
-                        <li className="nav-item">
-                            <span className="nav-link">Products</span>
-                        </li>
+                        {this.showMenus(menus)}
                     </ul>
                 </div>
             </nav>
         );
+    }
+    showMenus = (menus) => {
+        var result = null;
+        if (menus.length > 0) {
+            result = menus.map((menu, index) => {
+                return (
+                    <MenuLink
+                        key={index}
+                        label={menu.name}
+                        to={menu.to}
+                        activeOnlyWhenExact={menu.exact}
+                    />
+                );
+            });
+        }
+        
+        return result;
     }
 }
 export default Menu;
